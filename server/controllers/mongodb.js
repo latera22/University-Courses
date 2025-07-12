803f3858ce48214e2ab90237794c84a986348b61
Latera <laternigatu202@gmail.com>
2025-07-12 22:01:56 +0800

    editing mongoDb

--- server/controllers/mongodb.js
+++ server/controllers/mongodb.js
@@ -4,6 +4,9 @@ const ConnectDB = async () =>{
     mongoose.connection.on("connected", ()=>{
         console.log("Database connected")
     })
+    mongoose.connection.on("error", (err)=> {
+        console.log("Database connection error:", err.message)
+    })
     await mongoose.connect(`${process.env.MONGODB_URL}`);}
 
 export default ConnectDB
